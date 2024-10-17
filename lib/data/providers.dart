a44395dba905e2cf852cf459033761b1a900eeb3
Logan Kannan <logan842008@gmail.com>
2024-10-17 18:04:41 +0800

    latest

--- lib/data/providers.dart
+++ lib/data/providers.dart
@@ -1,5 +1,8 @@
+import 'package:fc_hackathon_2024/data/model/area.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authProvider = StateProvider<bool>((ref) => false);
 final loadingProvider = StateProvider<bool>((ref) => false);
 final rememberMeProvider = StateProvider<bool>((ref) => false);
+
+final selectedAreasProvider = StateProvider<List<Area>>((ref) => []);
